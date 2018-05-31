f97dce4f9546a13ac33c3f5d718a4f34be09fbea
Timothy Hill <tim@executivesuiteit.com>
2018-05-30 21:16:02 -0700

    initialize starting state for picksters plugin

---
+++ bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Description: Picksters fun stuff.
+ * Plugin Name:  Picksters
+ * Author: Timothy Hill
+ * Plugin URI:
+ * Version: 1.0.0
+ * @package ExecutiveSuiteIt\Picksters
+ * @Since 1.0.0
+ * @author Timothy Hill
+ * @link https://executivesuiteit.com
+ * @license
+ * requires WP: 4.9
+ * requires PHP: 7.1
+ */
+
+namespace ExecutiveSuiteIt\Picksters;
+
+function plugin_launch() {
+
+}
