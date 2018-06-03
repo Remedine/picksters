9f1186d56fc9a5110698d22462dbb8683f4647e3
Timothy Hill <tim@executivesuiteit.com>
2018-06-03 13:25:09 -0700

    added Picks and Games db tables to plugin registration hook

--- assets/init_plugin.php
+++ assets/init_plugin.php
@@ -24,18 +24,36 @@ register_activation_hook( __FILE__, 'init_create_custom_db_tables' );
 function init_create_custom_db_tables() {
 	global $wpdb;
 	//define the custom table name
-	$table_name = $wpdb->prefix . 'Pickster_teams';
+	$table_name = $wpdb->prefix . 'Pickster_picks';
 	//define the table structure
 	$sql = "CREATE TABLE " . $table_name . " (
-		id mediumint(3) NOT NULL AUTO_INCREMENT,
-		team_name VARCHAR(30) NOT NULL DEFAULT '',
-		team_city VARCHAR(50) NOT NULL DEFAULT '',
-		team_nickname_1 VARCHAR (20) NOT NULL DEFAULT '',
-		team_nickname_2 VARCHAR (20) DEFAULT ''
+		pick_id mediumint(30) AUTO_INCREMENT PRIMARY KEY,
+		user_name VARCHAR (30) NOT NULL,
+		user_id mediumint(30) NOT NULL,
+		pick_week mediumint(2) NOT NULL,
+		pick_year mediumint(4) NOT NULL,
+		game_id mediumint(30) NOT NULL,
+		home_team_name VARCHAR(30),
+		away_team_name VARCHAR(30),
+		pick VARCHAR(50) NOT NULL
+		);";
+
+	$table_name = $wpdb->prefix . 'Pickster_games';
+
+	$sql2 = "CREATE TABLE " . $table_name . " (
+		game_id mediumint(30) AUTO_INCREMENT PRIMARY KEY,
+		week mediumint(2) NOT NULL,
+		year mediumint(4) NOT NULL,
+		season_type VARCHAR(9) NOT NULL,
+		home_team_name VARCHAR(30),
+		away_team_name VARCHAR(30),
+		home_team_final_score mediumint(3) NOT NULL,
+		away_team_final_score mediumint(3) NOT NULL		
 		);";
 
 	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
 	//execute the query creating our table
 	dbDelta( $sql );
+	dbDelta( $sql2 );
 }
