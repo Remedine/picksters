c5d7dceb006ce386c7cb94903214b47294103d7b
Timothy Hill <tim@executivesuiteit.com>
2019-01-31 03:23:02 -0800

    added weekly picks meta-box to the weekly picks custom post type

--- classes/class_picksters_model_weekly_picks.php
+++ classes/class_picksters_model_weekly_picks.php
@@ -28,28 +28,50 @@ class Picksters_Model_Weekly_Picks {
 		$params['singular_post_name'] = __( 'Weekly Pick', 'picksters' );
 		$params['plural_post_name']   = __( 'Weekly Picks', 'picksters' );
 		$params['description']        = __( 'Make your weekly picks.', 'picksters' );
-		$params['supported_fields']   = array( 'title', 'editor' );
+		$params['supported_fields']   = array( 'title', 'custom-fields' );
 
 		$picksters->model_manager->create_post_type( $params );
 
 	}
 
 	public function add_weekly_picks_meta_boxes() {
-		add_meta_boxes( 'picksters_weekly_picks_meta', __( 'Weekly Pick Details', 'picksters' ), array(
+		add_meta_box( 'picksters_weekly_picks_meta', __( 'This Weeks Games', 'picksters' ), array(
 			$this,
-			'display_weekly_picks_meta_boxes,'
+			'display_weekly_picks_meta_boxes'
 		), $this->post_type );
 
 	}
 
+
 	public function display_weekly_picks_meta_boxes() {
 		global $post;
 
-		$html = "<table class='form-table>";
+		/*
+		$html = "<table class='form-table'>";
 		$html .= "<tr>";
-		$html .= "<th ><label><?php _e('Sticky Status', 'picksters'); ?>*</label></th>";
-		$html .= "<td><select class='widefat' name-'picksters_sticky_status'>";
-		$html .= "<option value='0'> <?php _e( 'Please Select', 'picksters' ); ?> </option>";
+		$html .= "<th ><label><?php _e('Sticky Status','picksters');
+			?>*</label></th>";
+		$html .= "<td><select class='widefat' name='picksters_sticky_status'
+			id='picksters_sticky_status'>";
+		$html .= "<option value='0' ><?php _e('Please Select','picksters');
+			?></option>";
+		$html .= "<option value='normal' ><?php _e('Normal','picksters'); ?>
+			</option>";
+		$html .= "<option value='super_sticky' ><?php _e('Super
+			Sticky','picksters'); ?></option>";
+		$html .= "<option value='sticky' ><?php _e('Sticky','picksters');
+			?></option>";
+		$html .= "</select></td>";
+		$html .= "</tr>";
+		$html .= "<tr>";
+		$html .= "<th ><label><?php _e('Topic Files','picksters');
+			?></label></th>";
+		$html .= "<td><input class='widefat' name='picksters_files'
+			id='picksters_files' type='file' value='' /></td>";
+		$html .= "</tr>";
+		$html .= "</table>";
+		echo $html;
+*/
 
 	}
 
@@ -75,6 +97,13 @@ class Picksters_Model_Weekly_Picks {
 		exit;
 	}
 
+	/**
+	 * Process the weekly-picks form data and save to database or push errors back to form.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
 	public function process_picks() {
 		global $picksters_weekly_picks_params;
 
@@ -87,8 +116,8 @@ class Picksters_Model_Weekly_Picks {
 			//make $Game[1-x] build variables dynamically depending upon how many games that week.
 			$how_many_games = $_POST['how_many_games'];
 			for ( $i = 1; $i <= $how_many_games; $i ++ ) {
-				${'game' . $i} = $_POST[ 'game' . $i ];
-				$picksters_weekly_picks_params['game' . $i ] = $_POST[ 'game' . $i ];
+				${'game' . $i}                                = $_POST[ 'game' . $i ];
+				$picksters_weekly_picks_params[ 'game' . $i ] = $_POST[ 'game' . $i ];
 
 
 				//push errors back to weekly picks template
@@ -98,6 +127,10 @@ class Picksters_Model_Weekly_Picks {
 			}
 
 			$picksters_weekly_picks_params['errors'] = $errors;
+
+			if ( empty( $errors ) ) {
+
+			}
 
 
 		}
