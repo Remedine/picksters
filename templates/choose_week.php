836a00228a16b494db17520645d227ca1f0368ea
Timothy Hill <tim@executivesuiteit.com>
2020-02-18 22:05:30 -0500

    added a choose week form

--- templates/choose_week.php
+++ templates/choose_week.php
@@ -15,7 +15,7 @@ get_header();
 
 ?>
 <div class="week_chooser">
-    <form action="welcome.php" method="post">
+    <form action="<?php echo get_site_url() . '/user/picks'; ?>" method="post" id="week_chooser_form">
         Name: <input type="text" name="name"><br>
         E-mail: <input type="text" name="email"><br>
         <input type="submit">
