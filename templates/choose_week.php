57e0ff5efde98b4394a1f4a0d8596a03965aa0c7
Timothy Hill <tim@digitalseeds.dev>
2020-02-22 17:44:28 -0500

    added choose_week template

--- templates/choose_week.php
+++ templates/choose_week.php
@@ -14,10 +14,34 @@ namespace ExecutiveSuiteIt\Picksters\Templates;
 get_header();
 
 ?>
+
 <div class="week_chooser">
     <form action="<?php echo get_site_url() . '/user/picks'; ?>" method="post" id="week_chooser_form">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
+
+        <label for="week">Choose a week:</label>
+        <select id="week" name="week" form="week">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+            <option value="16">16</option>
+            <option value="17">17</option>
+        </select>
+
         <input type="submit">
+
     </form>
 </div>
+
+	<?php get_footer(); ?>
